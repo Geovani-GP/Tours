9bac0555bdeef4ac775332e1d37d1ad7fd2b4833
Geovani <l.a.web@hotmail.com>
2025-04-04 03:07:43 -0600

    updt

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: './postcss.config.js'
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
   }
 })
